2b3792b02ca0e1492d9234d3135a9af447e09453
princewhyte2 <princewhyte02@gmail.com>
2021-10-01 23:30:23 +0100

    feat: dummy project

---
+++ scriptss.js
@@ -0,0 +1,59 @@
+const one = document.getElementById('one')
+one.addEventListener("mouseover", () => {
+    const oneP = document.getElementById('oneP')
+    const imgOne = document.getElementById('imgOne')
+    oneP.style.display = 'none'
+    imgOne.style.display = 'block'
+})
+
+one.addEventListener("mouseout", () => {
+    const oneP = document.getElementById('oneP')
+    const imgOne = document.getElementById('imgOne')
+    oneP.style.display = 'block'
+    imgOne.style.display = 'none'
+})
+
+const two = document.getElementById('two')
+two.addEventListener("mouseover", () => {
+    const twoP = document.getElementById('twoP')
+    const imgtwo = document.getElementById('imgtwo')
+    twoP.style.display = 'none'
+    imgtwo.style.display = 'block'
+})
+
+two.addEventListener("mouseout", () => {
+    const twoP = document.getElementById('twoP')
+    const imgtwo = document.getElementById('imgtwo')
+    twoP.style.display = 'block'
+    imgtwo.style.display = 'none'
+})
+
+const three = document.getElementById('three')
+three.addEventListener("mouseover", () => {
+    const threeP = document.getElementById('threeP')
+    const imgthree = document.getElementById('imgthree')
+    threeP.style.display = 'none'
+    imgthree.style.display = 'block'
+})
+
+three.addEventListener("mouseout", () => {
+    const threeP = document.getElementById('threeP')
+    const imgthree = document.getElementById('imgthree')
+    threeP.style.display = 'block'
+    imgthree.style.display = 'none'
+})
+
+const four = document.getElementById('four')
+four.addEventListener("mouseover", () => {
+    const fourP = document.getElementById('fourP')
+    const imgfour = document.getElementById('imgfour')
+    fourP.style.display = 'none'
+    imgfour.style.display = 'block'
+})
+
+four.addEventListener("mouseout", () => {
+    const fourP = document.getElementById('fourP')
+    const imgfour = document.getElementById('imgfour')
+    fourP.style.display = 'block'
+    imgfour.style.display = 'none'
+})
